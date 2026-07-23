64b44d48b1f2119fd049ae14079a276f6bb3876e
Abid Atmaja <abidatmajaa@gmail.com>
2026-07-23 13:19:24 +0700

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({ override: true });
+require('pg');
+require('pg-hstore');
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
